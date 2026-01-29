90a146dc9746a40a2e037560e161e75e663b91ed
SHAIKBASHI <74466145+SHAIKBASHI@users.noreply.github.com>
2026-01-29 08:53:47 +0530

    Add files via upload

---
+++ Arrays/TwoDimeArray.java
@@ -0,0 +1,17 @@
+package com.version2;
+
+public class TwoDimeArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[][]=new int [3][3];
+		for(int i=0;i<arr.length;i++) {
+			for(int j=0;j<arr.length;j++) {
+				System.out.println("index arr i= "+i+" j= "+j+" =>"+arr[i][j]);
+			//System.out.print(arr[i][j]);
+		}
+		}
+
+	}
+
+}
