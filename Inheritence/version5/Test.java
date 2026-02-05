aebff7f5704ba4830d69a62407ea54f0f384f834
SHAIKBASHI <74466145+SHAIKBASHI@users.noreply.github.com>
2026-02-05 21:22:08 +0530

    Add files via upload

---
+++ Inheritence/version5/Test.java
@@ -0,0 +1,12 @@
+package com.version5;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Employee obj=new Manager(11,"Manager",35000.00);
+		 
+		 System.out.println(obj.bonus());
+
+	}
+
+}
