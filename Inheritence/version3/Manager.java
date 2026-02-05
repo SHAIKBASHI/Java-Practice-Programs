9d630f8635127c8810e9c6ce545b981b8d8acb03
SHAIKBASHI <74466145+SHAIKBASHI@users.noreply.github.com>
2026-02-05 21:20:51 +0530

    Add files via upload

---
+++ Inheritence/version3/Manager.java
@@ -0,0 +1,11 @@
+package com.version3;
+
+public class Manager extends Employee {
+
+	public Manager(int id, String name, double salary) {
+		super(id, name, salary);
+		
+	}
+	
+
+}
---
+++ Inheritence/version3/Test.java
@@ -0,0 +1,12 @@
+package com.version3;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Manager obj=new Manager(11,"Manager",35000.00);
+		 
+		 System.out.println("Id="+obj.getId()+" Name="+obj.getName()+" Salary="+obj.getSalary());
+
+	}
+
+}
