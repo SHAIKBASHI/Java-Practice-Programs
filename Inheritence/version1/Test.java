bc94eed49a23a344a8be90a9c646cb46dc5d0428
SHAIKBASHI <74466145+SHAIKBASHI@users.noreply.github.com>
2026-02-05 21:19:41 +0530

    Add files via upload

---
+++ Inheritence/version1/Test.java
@@ -0,0 +1,12 @@
+package com.version1;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Manager obj=new Manager();
+		 obj.setId(11);obj.setName("Manager1");obj.setSalary(75000.00);
+		 System.out.println("ID="+obj.getId()+" Name="+obj.getName()+" Salary="+obj.getSalary());
+
+	}
+
+}
