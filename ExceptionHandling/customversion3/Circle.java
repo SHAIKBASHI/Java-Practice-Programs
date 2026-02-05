2507b4c349ab72f16437fb58fc477a039002aeb8
SHAIKBASHI <74466145+SHAIKBASHI@users.noreply.github.com>
2026-02-05 21:28:39 +0530

    Add files via upload

---
+++ ExceptionHandling/customversion3/Circle.java
@@ -0,0 +1,31 @@
+package com.custom.version3;
+
+public class Circle {
+	private double radius;
+	public Circle(double radius) {
+		this.radius=radius;
+		
+	}
+	public void printArea() throws RadiusException {
+		if(radius<0.0) {
+			
+				throw new RadiusException("Illegal Value");
+			
+		
+		}else {
+		double area=Math.PI*Math.pow(radius, 2);
+		System.out.println("Area of circle is "+area);}
+	}
+	public void printPeriMeter()  throws RadiusException {
+		if(radius<0.0)  {
+			
+				throw new RadiusException("Illegal Value");
+			
+		
+		}else {
+		double periMeter=2*Math.PI *radius;
+		System.out.println("Peri meter of circle is "+periMeter);}
+		
+	}
+  
+}
