84c7fb91cd30a1086decc7e538f27b1c378f3220
SHAIKBASHI <74466145+SHAIKBASHI@users.noreply.github.com>
2026-04-01 16:39:09 +0530

    Add files via upload

---
+++ Java8Features/version1/Functional.java
@@ -0,0 +1,14 @@
+package com.version1;
+@FunctionalInterface
+public interface Functional {
+	void show();
+	//void print(); gives error
+	default void go() {
+		
+		
+	}
+	static void run() {
+		
+	}
+
+}
